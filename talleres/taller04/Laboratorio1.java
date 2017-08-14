6e73ec6f71804f4c2582d55f6c04751de87a9827
eafit-201710093010 <30359351+eafit-201710093010@users.noreply.github.com>
2017-08-14 12:28:30 -0500

    Trabajo
    Aca trabajaremos en el taller 04, suba su parte del codigo desde el editor, con copiar y pegar

---
+++ talleres/taller04/Laboratorio1.java
@@ -0,0 +1,50 @@
+package laboratorio1;
+
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+public class Laboratorio1 {
+
+    public static void main(String[] args) {
+        
+        for (int i = 0; i < 1; i++) {
+            System.out.println( i+" " + tomarTiempo(100000));
+        }
+    }
+
+    public static void arraySum(int[] Sum) {
+        int c = 0;
+        for (int i = 0; i < Sum.length; i++) {
+            c = c + Sum[i];
+            try{
+           TimeUnit.MILLISECONDS.sleep(1);// delay de un milisegundo
+         }
+         catch(Exception e)
+         {
+         } 
+       }
+        //System.out.println(c);
+    }
+    
+    
+
+    public static int[] generarArregloDeTamanoN(int n) {
+        int max = 5000;
+        int[] array = new int[n];
+        Random generator = new Random();
+        for (int i = 0; i < n; i++) {
+            array[i] = generator.nextInt(max);
+        }
+        return array;
+    }
+
+    public static long tomarTiempo(int n) {
+        int[] a = generarArregloDeTamanoN(n);
+        long startTime = System.currentTimeMillis();
+        arraySum(a);
+        long estimatedTime = System.currentTimeMillis() - startTime;
+        return estimatedTime;
+    }
+}
+
+
