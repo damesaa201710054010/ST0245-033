4e335ecd735fc4876ad8770398a5e7145689ad10
Daniel Mesa <damesaa@eafit.edu.co>
2017-08-28 19:44:52 -0500

    Taller #5 terminado, PDF subido junto con el codigo final

--- talleres/taller05/java/Taller5Final.java
+++ talleres/taller05/java/Taller5Final.java
@@ -1,49 +1,50 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package Taller5;
-
-import java.util.Random;
-
+import java.util.*;
 /**
- *
- * @author danys
+ *Esta clase implementa los algoritmos de groupSum, Fibonacci y ArrayMax, todos con implementacion
+ *recursiva
+ *@author Kevin Arley Parra Henao, codigo: 201710093010, 
+ *        Daniel Alejandro Mesa Arango Codigo: 201710054010
+ *        Codigos de ArrayMax y Fibonacci tomados de los suministrados en el laboratorio 1
+ *@version 28/08/2017
+ * 
  */
-public class Taller5
+public class Taller5final
 {
-    public static boolean groupSum(int start, int[] nums, int target) 
+    //retorna true si este encuentra algun conjunto capaz de sumar el numero solicitado
+    public static boolean groupSum(int start, int [] nums, int target)
     {
-        if (start >= nums.length) return target == 0;
-        return groupSum(start + 1, nums, target - nums[start]) || groupSum(start + 1, nums, target);
+	if (start >= nums.length) return target == 0;
+	return groupSum(start+1, nums, target - nums[start])
+	    || groupSum(start+1, nums, target);
+	
     }
 
-    // Computes the maximum value of an array recursively
+    //Retorna el valor maximo de los elementos del arreglo
     public static int ArrayMax(int[] A, int n)
     {
-        int max;
-        if (n == 0)
-            max = A[0];
-        else
-            max = Math.max(A[n],ArrayMax(A, n-1));
-        return max;
+	int max;
+	if (n == 0)
+	    max = A[0];
+	else
+	    max = Math.max(A[n],ArrayMax(A, n-1));
+	return max;
     }
-  
-    // Computes Fibonnacci series recursively
-    public static int Fibonnacci(int n)
+
+    //Retorna el valor del Fibonacci para el valor ingresado
+    public static long Fibonnacci(int n)
     {
-        int result;
-        if (n == 0 || n == 1)
-            result = n;
-        else
-            result = Fibonnacci(n-1) + Fibonnacci(n-2);
-        return result;
+	long result;
+	if (n == 0 || n == 1)
+	    result = n;
+	else
+	    result = Fibonnacci(n-1) + Fibonnacci(n-2);
+	return result;
     }
-  
+
+    //Genera arreglos con valores aleatorios de tamaño n
     public static int[] generarArregloDeTamanoN(int n) 
     {
-        int max = 5000;
+	int max = 5000;
         int[] array = new int[n];
         Random generator = new Random();
         for (int i = 0; i < n; i++) 
@@ -52,60 +53,61 @@ public class Taller5
         }
         return array;
     }
-  
+
+    //Toma los tiempos de ejecucion para el algoritmo fibonacci
     public static long tomarTiempoFibo(int n) 
     {
         //int[] a = generarArregloDeTamanoN(n);
-        long startTime = System.currentTimeMillis();
+	long startTime = System.currentTimeMillis();
         System.out.println("Valor de Fibo: "+Fibonnacci(n));
         long estimatedTime = System.currentTimeMillis() - startTime;
         return estimatedTime;
     }
-   
+    
+    //Toma los tiempos de ejecucion para el algoritmo de GroupSum 
     public static long tomarTiempoGroup(int n) 
     {
-        int[] a = generarArregloDeTamanoN(n);
+	int[] a = generarArregloDeTamanoN(n);
+	Random gene = new Random();
+	int target = gene.nextInt(5000);
         long startTime = System.currentTimeMillis();
-        System.out.println("Valor de la suma: "+ArraySum(a, n-1));
+        System.out.println("Resultado: "+ groupSum(0, a, target));
         long estimatedTime = System.currentTimeMillis() - startTime;
         return estimatedTime;
     }
-   
+
+    //Toma los tiempos de ejecucion para el algoritmo de buscas el maximo
     public static long tomarTiempoMax(int n) 
     {
         int[] a = generarArregloDeTamanoN(n);
-        long startTime = System.currentTimeMillis();
-        System.out.println("Valor Maximo: "+ArrayMax(a, n-1));
+	long startTime = System.currentTimeMillis();
+        System.out.println("Valor Maximo: "+ ArrayMax(a, n-1));
+	//ArrayMax(a, n-1);
         long estimatedTime = System.currentTimeMillis() - startTime;
         return estimatedTime;
     }
-  
+
+    //implementa los metodos anteriores para 10 valores
     public static void main(String[] args)
     {
-        for(int i = 30; i <= 45; i = i + 5)
-        {
-            System.out.println("Tiempo en milisegundos: " + tomarTiempoFibo(i));
-        }
+	for(int i = 30; i <= 40; i++)
+	    {
+		System.out.println("Operacion: "+ i +" Tiempo en milisegundos: " + tomarTiempoFibo(i));
+		System.out.println();
+	    }
+
+	/*for(int i = 25; i <= 35  ; i++)
+	    {
+		System.out.println("Tiempo: "+i+" " + tomarTiempoGroup(i));
+		System.out.println();
+		}*/
       
-        /*for(int i = 100000; i <= 10000000; i = i*10)
-        {
-            System.out.println("Tiempo: " + tomarTiempoSum(i));
-        }*/
-      
-        for(int i = 100000; i <= 10000000; i = i*10)
-        {
-            System.out.println(tomarTiempoMax(i));
-        }
-      
-        /*int[] A = {1,5,3,2,6};
-        int[] B = {1,5,3,2};
-        int[] C = {1,5,2,6};
-        int [] D = {5};
-        int[][] Arrays = {A,B,C,D};   
-        for (int[] X : Arrays)
-        {        
-            System.out.println(Laboratorio1.ArraySum(X, X.length - 1) + " " + 
-            Laboratorio1.ArrayMax(X, X.length -1) + " " + Laboratorio1.Fibonnacci(X.length) +  "\n" );      
-        }*/
+	/*for(int i = 1000000; i <= 11000000; i = i+1000000)
+	    {
+		System.out.println(i +" " +tomarTiempoMax(i));
+		System.out.println();
+		}*/
+	
     }
+  
 }
