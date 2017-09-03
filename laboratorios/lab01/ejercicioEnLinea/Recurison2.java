2ce438055a84da31268800ebd718d61a10e881f8
eafit-201710093010 <30359351+eafit-201710093010@users.noreply.github.com>
2017-09-03 17:19:17 -0500

    Update and rename Recurison2 to Recurison2.java

--- laboratorios/lab01/ejercicioEnLinea/Recurison2.java
+++ laboratorios/lab01/ejercicioEnLinea/Recurison2.java
@@ -2,29 +2,43 @@ public class Recursion2 {
 
 //Metodo groupSum6, count6 y groumpSum tomado de: https://codingbatanswers.wordpress.com/recursion-2/
     public boolean groupSum6(int start, int[] nums, int target) {
-        if (start >= nums.length) {
+        if (start >= nums.length) {//C1
             return (target == 0);
         }
 
-        int diff = target - (6 * count6(start, nums));
-        if (groupSum(start, nums, diff)) {
+        int diff = target - (6 * count6(start, nums));//C2 + C3 + m
+        if (groupSum(start, nums, diff)) {//C4 + 2**n
             return true;
         }
         return false;
 
     }
+    
+    /** 
+     * Complejidad:
+     * T(n) = C1 + C2 +C3 + m + 2**n
+     * T(n) = C + n + 2**n
+     * luego T(n) es O(2**n)
+     */
 
     public int count6(int start, int[] nums) {
-        int count = 0;
-        if (nums[start] == 6) {
+        int count = 0;//C1
+        if (nums[start] == 6) {//C2
             count++;
             nums[start] = 0;
         }
-        if (start + 1 < nums.length) {
-            count += count6(start + 1, nums);
+        if (start + 1 < nums.length) {//C3
+            count += count6(start + 1, nums);//C4 + T(n-1)
         }
-        return count;
+        return count;//C5
     }
+    
+/**
+ * Complejidad
+ * T(n) = C1+ C2 + C3 + C4 + T(n-1) + C5
+ * T(n) = C + T(n-1)
+ * Luego T(n) es O(n)
+ */
 
 //this one (below) is the solution to groupSum but include it
 //in groupSum6
@@ -45,7 +59,7 @@ public class Recursion2 {
      * Complejidad 
      * T(n) = C1+C3+C5+2[T(n-1)] 
      * T(n) = C+2[T(n-1)] 
-     * luego T(n) es O(n)
+     * luego T(n) es O(2**n)
      */
     public boolean groupNoAdj(int start, int[] nums, int target) {
         if (start >= nums.length) {//C1
