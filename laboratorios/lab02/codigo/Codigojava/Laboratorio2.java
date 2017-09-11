93b50f46a1b37d68ac93e99e8d6ba1ecccaa3e9d
Daniel Mesa <damesaa@eafit.edu.co>
2017-09-10 19:26:18 -0500

    Borrador del informe codigos terminados, documentacion casi completa

---
+++ laboratorios/lab02/codigo/Codigojava/Laboratorio2.java
@@ -0,0 +1,168 @@
+import java.util.*;
+/**
+ * Laboratorio 2 de Estructuras de Datos 1. 
+ * Esta clase contiene los algoritmos de ArraySum, ArrayMax, InsertionSort y mergeSort, para calcular la suma de los elementos 
+ * de un arreglo, buscar el elemento mas grande en el arreglo, ordenar de menor a mayor los elementos de un arreglo y ordenar por el
+ * algoritmo de mergeSort los elementos del arreglo respectivamente, ademas los metodos para generar arreglos con un tamaño n con elementos
+ * aleatorios y tomar el tiempo de ejecucion de algoritmos.
+ * 
+ * @author Los algoritmos ArraySum, ArrayMax, InsertionSort y mergeSort fueron tomados del github proporcionado por el preoferor
+ * Toro, M (2017) Labpratorio 2 de Estrusturas de Datos (Version 1.0) 
+ * https://github.com/mauriciotoro/ST0245-Eafit/tree/master/laboratorios/lab02/codigo/Java/Laboratory1
+ * @author: Kevin Arley Parra Henao - Codigo: 201710093010, Daniel Alejandro Mesa Arango - Codigo: 201710054010 
+ * @version: 2.0 10/09/2017
+ */
+public class Laboratorio2 {
+    
+    /**
+     * Calcula la suma de todos los elementos del arreglo
+     * @param A el parametro anterior es el arreglo para el cual se quiere la suma de sus elementos y es de tipo entero
+     * @return retorna sum de tipo entero el cual es la suma de todos los elementos del arreglo
+     */
+    public static int ArraySum(int[] A) {
+        int sum = 0;
+        for (int i = 0; i < A.length; i++) {
+            sum = sum + A[i];
+        }
+        return sum;
+    }
+
+    /**
+     * Busca el mayor elemento en el arreglo
+     * @param A el arreglo en el cual se buscara el elemento mas grande, el arreglo es tipo entero
+     * @return retorna un entero el cual es el elemento de mayor tamaño en el arreglo
+     */
+    public static int ArrayMax(int[] A) {
+        int max = A[0];
+        for (int i = 0; i < A.length; i++) {
+            if (A[i] > max) {
+                max = A[i];
+            }
+        }
+        return max;
+    }
+
+    /**
+     * Ordena de menos a mayor los elementos del arreglo
+     * @param A el arreglo para el cual se ordenaran los elementos, el arreglo es de tipo entero
+     */
+    public static void InsertionSort(int[] A) {
+        int temp, j;
+        for (int i = 0; i < A.length; i++) {
+            j = i;
+            while (j > 0 && A[j - 1] > A[j]) {
+                temp = A[j];
+                A[j] = A[j - 1];
+                A[j - 1] = temp;
+                j = j - 1;
+            }
+        }
+        //System.out.println(Arrays.toString(A));
+    }
+
+    /**
+     * Este metodo hace uso de mergeSort para ordenar un arreglo
+     * @param a es el arreglo para el cual se quieren ordenar sus elementos y es de tipo entero
+     */
+    public static void mergeSort(int[] a) {
+        int[] tmp = new int[a.length];
+        mergeSort(a, tmp, 0, a.length - 1);
+    }
+        
+    /**
+     * Es un metodo recursivo
+     * @param a
+     * @param tmp
+     * @param left
+     * @param right 
+     */
+    private static void mergeSort(int[] a, int[] tmp, int left, int right) {
+        if (left < right) {
+            int center = (left + right) / 2;
+            mergeSort(a, tmp, left, center);
+            mergeSort(a, tmp, center + 1, right);
+            merge(a, tmp, left, center + 1, right);
+        }
+    }
+
+    /**
+     * 
+     * @param a
+     * @param tmp
+     * @param left
+     * @param right
+     * @param rightEnd 
+     */
+    private static void merge(int[] a, int[] tmp, int left, int right, int rightEnd) {
+        int leftEnd = right - 1;
+        int k = left;
+        int num = rightEnd - left + 1;
+
+        while (left <= leftEnd && right <= rightEnd) {
+            if (a[left] <= a[right]) {
+                tmp[k++] = a[left++];
+            } else {
+                tmp[k++] = a[right++];
+            }
+        }
+
+        while (left <= leftEnd) // Copy rest of first half
+        {
+            tmp[k++] = a[left++];
+        }
+
+        while (right <= rightEnd) // Copy rest of right half
+        {
+            tmp[k++] = a[right++];
+        }
+
+        // Copy tmp back
+        for (int i = 0; i < num; i++, rightEnd--) {
+            a[rightEnd] = tmp[rightEnd];
+        }
+    }
+
+    /**
+     * Toma los tiempos de ejecucion de un algoritmo
+     * @param n es un entero, el cual nos indica el tamaño del problema, en este caso especifico ya que siempre 
+     * se habla de arreglos, siendo entonces el tamaño del arreglo
+     * @return retorna un entero, el cual es el tiempo en milisegundos que se tardo un algoritmo en especifico en ejecutar
+     */
+    private static long tomarTiempos(int n)
+    {
+        int[] a = generarArregloDeTamanoN(n);
+        long startTime = System.currentTimeMillis();
+        /*System.out.println("Valor : "+  */ArrayMax(a);
+        long estimatedTime = System.currentTimeMillis() - startTime;
+        return estimatedTime;
+    }
+
+    /**
+     * Genera arreglos de tamaño n con numeros aleatorios entre 0 y 4999
+     * @param n es un entero, el cual indica el tamaño del arreglo a generar con numeros aleatorios
+     * @return un arreglo de enteros de tamaño n, con numeros aleatorios
+     */
+    private static int[] generarArregloDeTamanoN(int n) 
+    {
+        int max = 5000;
+        int[] array = new int[n];
+        Random generator = new Random();
+        for (int i = 0; i < n; i++) 
+        {
+            array[i] = generator.nextInt(max);
+        }
+        return array;
+    }
+
+    /**
+     * Metodo principal desde el cual se implementan los metodos de esta clase
+     * @param args 
+     */
+    public static void main(String[] args) {
+        for(int i = 100000; i <= 100000000; i = i*10)
+	    {
+		System.out.println("tiempo: "+i+": " + tomarTiempos(i));
+	    }
+    }
+
+}
