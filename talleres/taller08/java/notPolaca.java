4091987b8dcf44aae9ec4656ef357760dd7443d7
Daniel Mesa <damesaa@eafit.edu.co>
2017-09-25 22:01:13 -0500

    Punto 3 terminado

--- talleres/taller08/java/notPolaca.java
+++ talleres/taller08/java/notPolaca.java
@@ -1,27 +1,49 @@
 import java.util.*;
 public class notPolaca{
-    public int notacion()
+    public static int notacion()
     {
 	Scanner in = new Scanner(System.in);
-	String cadena = in.next();
+	String cadena = in.nextLine();
 	String [] partes = cadena.split(" ");
-	Stack<String> pila = new Stack<String>();
+	Stack<Integer> pila = new Stack<Integer>();
+	int result = 0;
 	for(int i = 0; i < partes.length; i++)
 	    {
-		pila.push(partes[i]);
-	    }
-	while(pila.emtpy())
-	    {
-		String afuera = pila.pop();
-		if(afuera == '+')
+		if(!(partes[i].equals("+")) && !(partes[i].equals("*")) && !(partes[i].equals("/"))
+		   && !(partes[i].equals("-")))
 		    {
-			
+			pila.push(Integer.parseInt(partes[i]));
+		    }else if(partes[i].equals("+"))
+		    {
+			result = pila.pop() + pila.pop();
+			pila.push(result);
+		    }else if(partes[i].equals("*"))
+		    {
+			result = pila.pop() * pila.pop();
+			pila.push(result);
+		    }else if(partes[i].equals("/")) 
+		    {
+			result = pila.pop() / pila.pop();
+			pila.push(result);
+		    }else if(partes[i].equals("-"))
+		    {
+			int primero = pila.pop();
+			int segundo = pila.pop();
+			if(primero > segundo)
+			    {
+				result = primero - segundo;
+			    }else
+			    {
+				result = segundo - primero;
+			    }
+			pila.push(result);
 		    }
 	    }
+	return pila.pop();
     }
 
     public static void main(String[] args)
     {
-	public notacion();
+	System.out.println(notacion());
     }
 }
