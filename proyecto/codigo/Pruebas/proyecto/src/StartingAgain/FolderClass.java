9f4396c4a66ceb86325943bbfb424dfbd0d0a399
Daniel Mesa <damesaa@eafit.edu.co>
2017-10-01 20:17:52 -0500

    Terminada version beta limpiare y agregare algunas cosas se añade igualmente los codigos de prueba

---
+++ proyecto/codigo/Pruebas/proyecto/src/StartingAgain/FolderClass.java
@@ -0,0 +1,101 @@
+package StartingAgain;
+
+import java.io.File;
+import java.util.LinkedList;
+
+public class FolderClass extends AbstractClass {
+
+    private String space;
+    private LinkedList<AbstractClass> archivos;
+    private String lastCarp;
+
+    public FolderClass(File file, String space) {
+        super(file);
+        archivos = new LinkedList<>();
+        generadorLinked(file.listFiles());
+        this.space = space;
+    }
+
+    public void generadorLinked(File[] file) {
+        for (int x = 0; x < file.length; x++) {
+            if (file[x].isFile()) {
+                archivos.add(new Archivo(file[x]));
+            } else if (file[x].isDirectory()) {
+                archivos.add(new FolderClass(file[x], space));
+            }
+        }
+    }
+
+    public void listar(String space) {
+        for (int x = 0; x < archivos.size(); ++x) {
+            if (archivos.get(x) instanceof Archivo) {
+                System.out.println(space + "Archivo: " + archivos.get(x).getNombre());
+            } else if (archivos.get(x) instanceof FolderClass) {
+                System.out.println(space + "Carpeta: " + archivos.get(x).getNombre());
+                String esp = space + "-";
+                FolderClass p = (FolderClass) archivos.get(x);
+                p.listar(esp);
+            }
+        }
+    }
+
+    public boolean eliminar(String name) {
+        for (int x = 0; x < archivos.size(); ++x) {
+            if (archivos.get(x) instanceof Archivo) {
+                if (archivos.get(x).getNombre().equals(name)) {
+                    System.out.println("Eliminando el archvio en:\n " + archivos.get(x).getFile());
+                    File aBorrar = archivos.get(x).getFile();
+                    aBorrar.delete();
+                    archivos.remove(x);
+                    return true;
+                }
+            } else if (archivos.get(x) instanceof FolderClass) {
+                if (archivos.get(x).getNombre().equals(name)) {
+                    System.out.println("Eliminando el archvio en:\n " + archivos.get(x).getFile());
+                    File aBorrar = archivos.get(x).getFile();
+                    aBorrar.delete();
+                    archivos.remove(x);
+                    return true;
+                } else {
+                    FolderClass p = (FolderClass) archivos.get(x);
+                    p.eliminar(name);
+                }
+            }
+
+        }
+        return false;
+    }
+
+    public String buscar(String name) {
+        String re = "";
+        for (int x = 0; x < archivos.size(); ++x) {
+            if (archivos.get(x) instanceof Archivo) {
+                if (archivos.get(x).getNombre().equals(name)) {
+                    lastCarp = archivos.get(x).getFile().getParent();
+                    re = ("El archvio existe:\n " + archivos.get(x).getFile());
+                }
+            } else if (archivos.get(x) instanceof FolderClass) {
+                if (archivos.get(x).getNombre().equals(name)) {
+                    lastCarp = archivos.get(x).getFile().toString();
+                    re = ("El archvio existe:\n " + archivos.get(x).getFile());
+                    break;
+
+                } else {
+                    FolderClass p = (FolderClass) archivos.get(x);
+                    re = p.buscar(name);                            
+                }
+            }
+
+        }
+        return re;
+    }
+
+    public String obtLastDir() {
+        return lastCarp;
+    }
+
+    public boolean crear(String dir) {
+        return false;
+    }
+
+}
---
+++ proyecto/codigo/Pruebas/proyecto/src/StartingAgain/StartingAgain.java
@@ -0,0 +1,93 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package StartingAgain;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ *
+ * @author evinracher
+ */
+public class StartingAgain {
+
+    private String sDirectorio;
+    private String OS;
+    private String OSArch;
+    private String OSversion;
+    private String OSdataModel;
+    private FolderClass prueba;
+    private File Last;
+
+    public StartingAgain() {
+        definiendoSistema();
+        loading();
+        File f = new File(sDirectorio);
+        prueba = new FolderClass(f, "");
+    }
+
+    public void consola() throws IOException {
+        BufferedReader teclado = new BufferedReader(new InputStreamReader(System.in));
+        String comando = teclado.readLine();
+        while (!(comando.equals("exit"))) {
+            if (comando.equals("ls")) {
+                System.out.println("\nListando elementos del directorio: " + sDirectorio);
+                prueba.listar("");
+
+            }
+            if (comando.equals("buscar")) {
+                System.out.println("Ingrese el nombre del archivo o carpeta a "
+                        + "buscar");
+                String name = teclado.readLine();
+                String resultado = prueba.buscar(name);
+                if (resultado.equals("")) {
+                    System.out.println("El archivo no existe");
+                } else {
+                    System.out.println(resultado);
+                }
+            }
+            if (comando.equals("rm")) {
+                System.out.println("Ingrese el nombre del archivo o carpeta a "
+                        + "eliminar");
+                String name = teclado.readLine();
+                if (!prueba.eliminar(name)) {
+                    System.out.println("El archvio no existe\n");
+                }
+            }
+            if (comando.equals("mkdir")) {
+                System.out.println("Padre con Parent: " + prueba.obtLastDir());
+            }
+            comando = teclado.readLine();
+        }
+        System.out.println("logout");
+    }
+
+    public void loading() {
+        System.out.println("BIENVENIDO\nDatos del sistema:\n"
+                + "Sistema Operativo: " + OS
+                + "\nArquitectura: " + OSArch
+                + "\nVersion: " + OSversion
+                + "\nData Model: " + OSdataModel);
+    }
+
+    public void definiendoSistema() {
+        OS = System.getProperty("os.name").toLowerCase();
+        OSArch = System.getProperty("os.arch").toLowerCase();
+        OSversion = System.getProperty("os.version").toLowerCase();
+        OSdataModel = System.getProperty("sun.arch.data.model");
+
+        if (OS.equals("linux")) {
+            sDirectorio = "/home/evinracher/prueba";
+        } else if (OS.substring(0, 7).equals("windows")) {
+            sDirectorio = "C:\\Prueba";
+        } else if (OS.equals("mac")) {
+            System.out.println("No tenemos mac :c");
+        }
+
+    }
+}
